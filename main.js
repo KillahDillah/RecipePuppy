2bfac8fd486caa6d0abc218518a220a58e8b5e3d
Leah Miller <leahmiller1969@gmail.com>
2017-07-18 14:49:14 -0700

    bugs fixed

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 
 let main = document.querySelector(".container")
 let search = document.querySelector("#search")
+let form = document.querySelector('#button')
+
+form.addEventListener("click", function(e){
+	e.preventDefault()
+
+	var term = search.value
+
+	find(term)
+})
 
 
 function find(term) {
-	search.value.addEventListener("click",function(){
-
+	//search.value.addEventListener("click",function(){
+	//document.getElementById("button").addEventListener("click", myFunction);
+	console.log(term)
 	fetch("http://www.recipepuppy.com/api/?q=" + term)
 	.then (
 		function(response){
@@ -13,21 +23,27 @@ function find(term) {
 				console.log(response.status)
 				return;
 			}
-		response.json().then(function(data){
-			data.results.map(function(info){
-				let list = `
-					<div class="row"> 
-					<a href="${info.href}">${info.title}</a>
-					<li> ${info.ingredients}</li>
-					<img src=" ${info.thumbnail} ">
-					</div>
-				`
-				main.innerHTML += list
-			})
+			response.json().then(function(data){
+				let list = ''
+				data.results.map(function(info){
+					
+					if (!info.thumbnail){
+						info.thumbnail = "http://via.placeholder.com/200x200"
+					}
+					list += `
+						<div class="row"> 
+						<a href="${info.href}"><img src=" ${info.thumbnail}"/></a>
+						<h4>${info.title}</h4>
+						<li> ${info.ingredients}</li>
+						</div>
+					`	
+				})
+
+	
+				main.innerHTML = list
 
 			})
 
 		}
 	)
-})
 }
